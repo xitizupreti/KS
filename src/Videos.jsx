77e4deddb88589aa40400a6ecaf277ea2b047ae3
Kshitiz Upreti <upreti.kshitiz.ku@gmail.com>
2022-12-15 01:28:54 +0545

    preload

--- src/Videos.jsx
+++ src/Videos.jsx
@@ -17,15 +17,15 @@ const Videos = () => {
       {/* {images.map((val) => {
         return <img src={val.images[0]} />;
       })} */}
-      <video className="vid" controls><source src={videos[0]}/> </video>
-      <video className="vid" controls><source src={videos[1]}/> </video>
-      <video className="vid" controls><source src={videos[2]}/> </video>
-      <video className="vid" controls><source src={videos[3]}/> </video>
-      <video className="vid" controls><source src={videos[4]}/> </video>
-      <video className="vid" controls><source src={videos[5]}/> </video>
-      <video className="vid" controls><source src={videos[6]}/> </video>
-      <video className="vid" controls><source src={videos[7]}/> </video>
-      <video className="vid" controls><source src={videos[8]}/> </video>
+      <video className="vid" preload="metadata" controls><source src={videos[0]}/> </video>
+      <video className="vid" preload="metadata" controls><source src={videos[1]}/> </video>
+      <video className="vid" preload="metadata" controls><source src={videos[2]}/> </video>
+      <video className="vid" preload="metadata" controls><source src={videos[3]}/> </video>
+      <video className="vid" preload="metadata" controls><source src={videos[4]}/> </video>
+      <video className="vid" preload="metadata" controls><source src={videos[5]}/> </video>
+      <video className="vid" preload="metadata" controls><source src={videos[6]}/> </video>
+      <video className="vid" preload="metadata" controls><source src={videos[7]}/> </video>
+      <video className="vid" preload="metadata" controls><source src={videos[8]}/> </video>
       </div>
     </>
   );
